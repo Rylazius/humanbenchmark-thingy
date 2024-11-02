8e0f746ae9523a805002e5600cb71b0c39e960b2
r <94881672+Rylazius@users.noreply.github.com>
2024-11-02 19:50:43 +0700

    ez

---
+++ aim.js
@@ -0,0 +1,7 @@
+for (i = 0; i < 31; i++) {
+  document.getElementsByClassName("css-z6vxiy e6yfngs3")[0].dispatchEvent(new MouseEvent('mousedown', {
+    bubbles: true,
+    cancelable: true,
+    button: 0
+  }))
+}
